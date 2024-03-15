42be4debae3dc4da303288dc6c134f90fff19dbb
timothydanfield <timothydanfield@yahoo.com>
2024-03-15 10:57:39 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar/Navbar"; // Assuming Navbar.js is in the same directory
-
+import Shop from "Shop.js";
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Shop />
       </div>
     </Router>
   );
